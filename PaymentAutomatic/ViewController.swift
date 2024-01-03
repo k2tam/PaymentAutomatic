eedde8568450f14656b19a89a6e4716117aca522
Tam Bui <buingockhaitam01@gmail.com>
2024-01-03 11:54:32 +0700

    first commit

---
+++ PaymentAutomatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaymentAutomatic
+//
+//  Created by k2 tam on 03/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
